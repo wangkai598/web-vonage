4f206814f8ce541b411d191fd2ff92cd29f9a56b
wangkai <wangkai@shanda.com>
2021-11-30 18:47:36 +0800

    q

--- js/app.js
+++ js/app.js
@@ -15,43 +15,110 @@ function handleError(error) {
   
   function initializeSession() {
     var session = OT.initSession(apiKey, sessionId);
-    let audioInputDevices;
-    OT.getDevices(function(error, devices) {
-        audioInputDevices=devices.filter(function(element) {
-            console.log(element);
-            return element.kind == "audioInput";
-        });
+    console.log('初始化session对象',session)
+
+    //初始化OT session对象失败
+    // OT.addEventListener("exception", event=>{
+    //     console.log("exception event. \n  code == " + event.code + "\n  message == " + event.message);
+    //     if(callInfo.operatorType == 1){//加入者
+    //         hang(5);
+    //     }else{
+    //         hang(4);
+    //     }
+    // });
+
+    session.on('streamPropertyChanged', (event)=> {
+        console.log('streamPropertyChanged',event)
+
+
     });
-    // Subscribe to a newly created stream
-    session.on('streamCreated', function(event) {
 
-        var a = session.subscribe(event.stream, 'subscriber', {
-          insertMode: 'append',
-          width: '100%',
-          height: '100%',
-          audioVolume:100
-        }, handleError);
-        a.subscribeToAudio(true);
-      });
+   session.on("sessionDisconnected", (event)=> {
+        console.log('sessionDisconnected',event)
+    });
 
-    // Create a publisher
-    let publisherOptions = {
-        insertMode: 'append',
-        width: '100%',
-        height: '100%',
-        audioBitrate: 64000
-      };
-      if(audioInputDevices)
-          publisherOptions.audioSource = audioInputDevices.deviceId;
-    var publisher = OT.initPublisher('publisher', publisherOptions, handleError);
+    
+    session.on('connectionCreated',(event)=>{
+        console.log('connectionCreated',event);
+    })
+
+
+    session.on('connectionDestroyed',(event)=>{
+        console.log('链接中断');
+    })
+    
+
+    session.on('sessionReconnecting',(event)=>{//正在重连
+        console.log('进入重连')
+
+    })
+
+
+    session.on('sessionReconnected',(event)=>{//重连成功
+        console.log('重连成功',event) 
+    });
   
+    // initialize the publisher
+    let publisherOptions = {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%',
+      audioBitrate: 64000,
+    //   videoSource: videoInputDevices[0].deviceId
+    };
+    // if(audioInputDevices)
+    //     publisherOptions.audioSource = audioInputDevices.deviceId;
+
+    // console.log(audioInputDevices)
+    publisher = OT.initPublisher('publisher', publisherOptions, handleError);
+    console.log('初始化publisher对象',publisher)
+
+    publisher.publishAudio(true);
+    publisher.publishVideo(true);
+
     // Connect to the session
-    session.connect(token, function(error) {
-      // If the connection is successful, publish to the session
-      if (error) {
-        handleError(error);
-      } else {
-        session.publish(publisher, handleError);
-      }
+    session.connect(token, (error)=> {//连接 错误信息返回
+        if (error) {
+            console.log('连接失败',error)
+            handleError(error);
+        } else {
+            console.log('链接成功')
+            session.publish(publisher,(error)=>{
+                if (error) {
+                    console.log('publisher_error',error);
+                }
+            });
+        }
     });
+
+    publisher.on('mediaStopped', (event)=> {
+        // if(event.type == 'mediaStopped' && callState == 'connected' && callInfo.callType == 1){
+        //     _switchCall(0,1010);
+        // }
+        console.log(event)
+    });
+
+        publisher.on('streamCreated', (event)=> {
+            console.log('加入者推流成功')
+        });
+
+        session.on('streamCreated', (event)=> {
+            let subscriberOptions = {
+                insertMode: 'append',
+                width: '100%',
+                height: '100%',
+                audioVolume:100
+            };
+            // session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+            subscriber = session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+            // subscriber.subscribeToVideo(true);
+            subscriber.subscribeToAudio(true);
+        });
+
+
+    publisher.on("streamDestroyed", function (event) {
+        console.log("The publisher stopped streaming. Reason: "
+          + event.reason);
+    });
+
   }
